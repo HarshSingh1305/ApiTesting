64daaccc3e8af40ffc3dcbb620bc6ebec92d0dc4
HarshSingh1305 <96624284+HarshSingh1305@users.noreply.github.com>
2022-04-03 21:20:41 +0530

    Update DatabaseMailtriggerScript.py

--- DatabaseMailtriggerScript.py
+++ DatabaseMailtriggerScript.py
@@ -1,29 +1,37 @@
-from email.mime.multipart import MIMEMultipart
-from email.mime.application import MIMEApplication
-from email.mime.text import MIMEText
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+mail_content = '''PFA for the DailyGenix Database attachment,'''
 
 
-def send_mail():
-    # Create a multipart message
-    msg = MIMEMultipart()
-    body_part = MIMEText(MESSAGE_BODY, 'plain')
-    msg['Subject'] = 'GetDataAnytime'
-    msg['From'] = 'dailygenixauto@gmail.com'
-    msg['To'] = 'msdhamija@yahoo.co.in,harshdhiman01@gmail.com'
-    # Add body to email
-    msg.attach(body_part)
-    
-    # open and read the CSV file in binary
-    with open('/__w/ApiTesting/ApiTesting/getDataAnytime.csv','rb') as file:
-    # Attach the file with filename to the email
-        msg.attach(MIMEApplication(file.read(), Name=getDataAnytime))
+sender_address = 'dailygenixauto@gmail.com'
+sender_pass = 'H@rshsingh1305'
+receiver_address = 'harshdhiman01@gmail.com,msdhamija@yahoo.co.in'
 
-    # Create SMTP object
-    smtp_obj = smtplib.SMTP('smtp.gmail.com', 587)
-    # Login to the server
-    smtp_obj.login('dailygenixauto@gmail.com', 'H@rshsingh1305')
 
-    # Convert the message to a string and send it
-    smtp_obj.sendmail(msg['From'], msg['To'], msg.as_string())
-    smtp_obj.quit()
+message = MIMEMultipart()
+message['From'] = sender_address
+message['To'] = receiver_address
+message['Subject'] = 'A test mail sent by Python. It has an attachment.'
+
+attach_file_name = '/__w/ApiTesting/ApiTesting/getDataAnytime.csv'
+attach_file = open(attach_file_name, 'rb') # Open the file as binary mode
+payload = MIMEBase('application', 'octate-stream')
+payload.set_payload((attach_file).read())
+encoders.encode_base64(payload) #encode the attachment
+#add payload header with filename
+payload.add_header('Content-Decomposition', 'attachment', filename=attach_file_name)
+message.attach(payload)
+
+
+message.attach(MIMEText(mail_content, 'plain'))
+#Create SMTP session for sending the mail
+session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+session.starttls() #enable security
+session.login(sender_address, sender_pass) #login with mail_id and password
+text = message.as_string()
+session.sendmail(sender_address, receiver_address, text)
+session.quit()
+print('Mail Sent')
